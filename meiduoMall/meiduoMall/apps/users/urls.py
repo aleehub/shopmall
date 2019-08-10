b4536bfd782c10bd4ede43035b6bf535ff77fae2
aleehub <37734923+aleehub@users.noreply.github.com>
2019-08-10 22:35:49 +0800

    设置用户注册视图

--- meiduoMall/meiduoMall/apps/users/urls.py
+++ meiduoMall/meiduoMall/apps/users/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
+
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
 
-    url(r'^user/', include('users.urls', namespace="users")),
+    url(r'^register/$', views.RegisterView.as_view())
 ]
--- meiduoMall/meiduoMall/apps/users/views.py
+++ meiduoMall/meiduoMall/apps/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+class RegisterView(View):
+    """类视图：处理注册"""
+
+    def get(self, request):
+        """处理GET请求，返回注册页面"""
+        return render(request, 'register.html')
+
+    def post(self, request):
+        """ 处理POST请求：实现注册逻辑"""
+        return HttpResponse('这里实现注册逻辑')
