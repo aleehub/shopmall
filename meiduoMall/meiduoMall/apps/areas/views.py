eff739320ca1d01a55b992bd5357a6f573956f07
aleehub <37734923+aleehub@users.noreply.github.com>
2019-08-20 14:58:43 +0800

    用户地址视图传回地址数据

--- meiduoMall/meiduoMall/apps/areas/views.py
+++ meiduoMall/meiduoMall/apps/areas/views.py
@@ -1,11 +1,73 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 
+from meiduoMall.utils.response_code import RETCODE
+from .models import Area
+import logging
 
+logger = logging.getLogger('django')
 # Create your views here.
 
 
 class AreasView(View):
 
     def get(self, request):
-        pass
+        """提供省市区数据"""
+
+        area_id = request.GET.get('area_id')
+
+        if not area_id:
+            # 提供省份数据
+
+            try:
+                # 查询省份数据
+
+                province_model_list = Area.objects.filter(parent=None)
+
+                # 序列化省份数据
+
+                province_list = []
+
+                for province_model in province_model_list:
+                    province_list.append({'id': province_model.id, 'name': province_model.name})
+
+            except Exception as e:
+
+                logger.error(e)
+
+                return JsonResponse({'code': RETCODE.DBERR, 'errmsg': '省份数据错误'})
+
+            # 响应省份数据
+            return JsonResponse({'code': RETCODE.OK, 'errmsg': 'ok', 'province_list': province_list})
+
+        else:
+            # 提供市或区数据
+
+            try:
+
+                parent_model = Area.objects.get(id=area_id)
+
+                sub_model_list = parent_model.subs.all()
+
+                # 序列化市区数据
+
+                sub_list = []
+
+                for sub_model in sub_model_list:
+                    sub_list.append({'id': sub_model.id, 'name': sub_model.name})
+
+                sub_data = {
+                    'id': parent_model.id,  # 父级pk
+                    'name': parent_model.name,  # 父级name
+                    'subs': sub_list,  # 父级的子集
+                }
+
+            except Exception as e:
+
+                logger.error(e)
+
+                return JsonResponse({'code': RETCODE.DBERR, 'errmsg': '市区数据错误'})
+
+            # 响应市区数据
+            return JsonResponse({'code': RETCODE.OK, 'errmsg': 'ok', 'sub_data': sub_data})
